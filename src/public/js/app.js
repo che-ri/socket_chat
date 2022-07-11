2e874eee3c8806b2ef8f3ab1a08242a4c2c0cc7c
che-ri <rnjs1135@gmail.com>
2022-07-12 04:15:26 +0900

    feat ::: websocket => socket.io 로 마이그레이션

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,34 +1,30 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+// const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io(); //socket.io를 설치하면 io()를 사용할 수 있다.
 
 const $message_ul = document.querySelector("ul");
 const $message_form = document.querySelector("#message");
 const $nickname_form = document.querySelector("#nickname");
 
-socket.addEventListener("open", () => console.log("connect to server"));
-socket.addEventListener("message", (m) => {
-    const $li = document.createElement("li");
-    $li.textContent = m.data;
-    console.log(m);
-    $message_ul.append($li);
-});
-socket.addEventListener("close", () => console.log("disconnect to server"));
-
-function makeSocketRequest(type, payload) {
-    return JSON.stringify({ type, payload });
-}
-
 $message_form.addEventListener("submit", (event) => {
     event.preventDefault();
     const $input = $message_form.querySelector("input");
-
     //socket애는 string형식만 보낼 수 있기 때문에, object => string 형식으로 변환
-    socket.send(makeSocketRequest("message", $input.value));
+    socket.emit("message", $input.value);
     $input.value = "";
 });
 
 $nickname_form.addEventListener("submit", (event) => {
     event.preventDefault();
     const $input = $nickname_form.querySelector("input");
-    socket.send(makeSocketRequest("nickname", $input.value));
+    socket.emit("nickname", $input.value);
     $input.value = "";
 });
+
+socket.on("message", (message) => {
+    const $li = document.createElement("li");
+    $li.textContent = message;
+
+    $message_ul.append($li);
+});
+
+socket.on("disconnect", () => console.log("disconnect to server"));
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIo from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,30 +10,19 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("home"));
 
-//아래는 http서버와 websocket서버를 한개로 합치기 위한 작업이다. express에서는 ws를 지원하지 않기 때문에, http의 createServer을 이용하여 따로 서버를 만들어주어야한다.
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = SocketIo(server);
 
-//채팅방에 입장한 사람들은 소켓에 연결될 것이고, 연결된 소켓은 이 array에 들어간다.
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "익명";
-    socket.on("close", () => console.log("disconnected from the browser"));
+io.on("connection", (socket) => {
+    let user_name = "익명";
     socket.on("message", (message) => {
-        //클라이언트로부터 받은 string 형식의 객체를 parse
-        const parsed_message = JSON.parse(message.toString());
-
-        switch (parsed_message.type) {
-            case "message":
-                sockets.forEach((s) =>
-                    s.send(`${socket.nickname}: ${parsed_message.payload}`)
-                );
-            case "nickname":
-                //socket은 기본적으로 객체라, 키를 추가할 수 있음. socket 안에 사용자를 정의할 수 있는 닉네임을 실어서 보낸다.
-                socket["nickname"] = parsed_message.payload;
-        }
+        io.emit("message", `${user_name}: ${message}`);
+    });
+    socket.on("nickname", (nickname) => {
+        user_name = nickname;
+    });
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
     });
 });
 
